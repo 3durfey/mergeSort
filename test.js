444a5a880d7c4016912dddb39fc19392dbc0f05d
pdurfey <3durfey@gmail.com>
2023-06-11 14:52:15 -0400

    reduced size

--- test.js
+++ test.js
@@ -1,40 +1,17 @@
 function mergeSort(unsorted) {
   let array1, array2;
-  if (unsorted.length >= 3) {
+  if (unsorted.length > 1) {
     array1 = mergeSort(unsorted.slice(0, unsorted.length / 2));
     array2 = mergeSort(unsorted.slice(unsorted.length / 2));
-  } else {
-    return sortBaseCase(unsorted);
-  }
+  } else return unsorted;
   return merge(array1, array2);
 }
-
 function merge(insertArray, array) {
   let index = 0;
   for (x in insertArray) {
-    if (insertArray[x] < array[index]) {
-      array = insert(index, insertArray[x], array);
-    } else {
-      while (insertArray[x] > array[index] && index <= array.length - 1) {
-        index++;
-      }
-      if (insertArray[x] !== array[index])
-        array = insert(index, insertArray[x], array);
-    }
-  }
-  return array;
-}
-
-function insert(index, input, array) {
-  let arr = [...array.slice(0, index), input, ...array.slice(index)];
-  return arr;
-}
-function sortBaseCase(array) {
-  if (array.length === 1) return array;
-  if (array[0] > array[1]) {
-    let temp = array[1];
-    array[1] = array[0];
-    array[0] = temp;
+    while (insertArray[x] > array[index] && index <= array.length - 1) index++;
+    if (insertArray[x] !== array[index])
+      array = [...array.slice(0, index), insertArray[x], ...array.slice(index)];
   }
   return array;
 }
